0f06bcd0ce3f25c4c03f7971644c7a1c679f5e2d
kamihiro0626 <hiroya.kamishioiri@gmail.com>
2020-08-24 15:23:22 +0900

    バリデーション の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :status, :shipping_fee_burden, :shipping_origin, :shipping_days
+  belongs_to :user
+
+
+  with_options presence: true do
+    validates :name, :image, :content, :category, :status, :shipping_fee_burden, :shipping_origin, :shipping_days, :price, :user
+  end
+
+ with_options numericality: { other_than: 1 } do
+  validates :category, :status, :shipping_fee_burden, :shipping_origin, :shipping_days
+ end
 end
