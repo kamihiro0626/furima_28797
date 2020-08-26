cd879d55699598b869cc69e9325ad74e0c49f975
kamihiro0626 <hiroya.kamishioiri@gmail.com>
2020-08-26 11:54:19 +0900

    非同期の設定（手数料、利益）

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../../views/items/new")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
