dbe12b237ef1ffca668ab4bde1f7de6af715574f
kamihiro0626 <hiroya.kamishioiri@gmail.com>
2020-08-29 15:40:31 +0900

    レビュー補正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,7 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @seller = @item.user.nickname
   end
 
@@ -34,5 +34,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :image, :content, :category_id, :status_id, :shipping_fee_burden_id, :shipping_origin_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
