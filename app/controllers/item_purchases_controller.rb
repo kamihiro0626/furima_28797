d6c44f1bd0a0ae0a65c895e161f9629f6f2439c7
kamihiro0626 <hiroya.kamishioiri@gmail.com>
2020-09-01 17:04:40 +0900

    createアクションの作成

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,14 +1,41 @@
 class ItemPurchasesController < ApplicationController
    before_action :move_to_index
+   attr_accessor :token
+
   def show
     @item_purchase = Item.find(params[:id])
   end
+
+  def create
+    @purchase = ItemPurchase.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'show'
+    end
+  end
+
+  private
 
   def move_to_index
     unless user_signed_in?
       redirect_to new_user_session_path
     end
   end
-  # def create
-  # end
+
+  def purchase_params
+    params.require(:item_purchase).permit(:user_id, :item_id, :token)
+  end
+
+ def pay_item
+   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+   Payjp::Charge.create(
+     amount: purchase_params[:item_id.price],  # 商品の値段
+     card: purchase_params[:token],    # カードトークン
+     currency:'jpy'                 # 通貨の種類(日本円)
+   )
+ end
+  
 end
--- app/models/item_purchase.rb
+++ app/models/item_purchase.rb
@@ -1,4 +1,10 @@
 class ItemPurchase < ApplicationRecord
   belongs_to :user
   belongs_to :item
+
+ with_options presence: true do
+   validates :user_id
+   validates :item_id
+   validates :token
+ end
 end
