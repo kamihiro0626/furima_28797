023c6c6ce8d2f05b6d9aa98e3c1774b93c9c4b9f
kamihiro0626 <hiroya.kamishioiri@gmail.com>
2020-09-10 19:15:33 +0900

    自動デプロイ補正3

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, furima_28797
+set :application, 'furima_28797'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:kamihiro0626/furima_28797.git'
